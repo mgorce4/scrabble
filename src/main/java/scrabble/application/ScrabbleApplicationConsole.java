498130790c4249486aa94778a3088c8d4c88b98b
Julie Courty <julielanngoc10@gmail.com>
2024-04-09 16:03:10 +0200

    Commit pour tester le pull ;)

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -5,6 +5,7 @@ public class ScrabbleApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("-------------------------------------------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+		System.out.println("-- Développé par nous --");
 		System.out.println("-------------------------------------------------------");
 
 	}
