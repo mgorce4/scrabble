dbe4e3c61d6d34b627ea35f9d90a20606a93db16
Marius Gambart--Guiguen <marius.gambartguiguen@etu.unilim.fr>
2024-04-30 11:48:43 +0200

    test plateau

---
+++ src/main/java/scrabble/matériel/Plateau.java
@@ -0,0 +1,48 @@
+package scrabble.matériel;
+
+public class Plateau {
+	
+
+	private int nbLig;
+	private int nbCol;
+	private char [][] plateau;
+	
+
+	public Plateau (int n, int p){
+		nbLig = n;
+		nbCol = p;
+		plateau = new char[nbLig][nbCol];
+
+		for(int i = 0; i<nbLig; i++) {
+			for(int j = 0; j<nbCol; j++) {
+				
+
+				plateau[i][j] = 'x';				
+
+			}
+		}
+	}
+	
+
+	
+
+	
+	public void afficher(){
+		System.out.println();
+		for (int i=0; i<nbLig; i++) {
+			for (int j=0; j<nbCol; j++) {
+				
+				System.out.print(" | " + plateau[i][j]);
+				
+				
+				
+			}
+			System.out.println(" |");
+		}
+		System.out.println();
+	}
+}
+
+
+
+
