5799e6f4d62dce82a85dc2b2ec5425e18dcde484
Max Gorce <max.gorce@yahoo.com>
2024-05-24 14:06:57 +0200

    création classe ChevaletTest

---
+++ src/test/java/scrabble/matériel/ChevaletTest.java
@@ -0,0 +1,14 @@
+package scrabble.matériel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChevaletTest {
+
+	@Test
+	void test() {
+		//TODO
+	}
+
+}
