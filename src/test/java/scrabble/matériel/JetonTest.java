6ddef9826e3b722c6f21bfe2f54128568882e8c7
Max Gorce <max.gorce@yahoo.com>
2024-05-29 08:17:11 +0200

    tests pour la classe jeton

---
+++ src/test/java/scrabble/matériel/JetonTest.java
@@ -0,0 +1,22 @@
+package scrabble.matériel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JetonTest {
+
+    @Test
+    public void testJeton() {
+        Jeton jeton = new Jeton(Lettre.A, Point.UN);
+
+        assertEquals(Lettre.A, jeton.lettre());
+        assertEquals(Point.UN, jeton.point());
+    }
+
+    @Test
+    public void testToString() {
+        Jeton jeton = new Jeton(Lettre.A, Point.UN);
+        assertEquals("|a|  pt:1", jeton.toString());
+    }
+}
