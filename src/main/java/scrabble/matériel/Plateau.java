88e4214a1d7ccf14397a4be2920263e992a696cc
Max Gorce <max.gorce@yahoo.com>
2024-05-30 14:22:54 +0200

    maj correcte creerPlateau

--- src/main/java/scrabble/matériel/Plateau.java
+++ src/main/java/scrabble/matériel/Plateau.java
@@ -1,88 +1,37 @@
-/* package scrabble.matériel;
-
-public class Plateau {
-	
-
-	private static int nbLig;
-	private static int nbCol;
-	private char [][] plateau;
-	
-
-	public Plateau (int n, int p){
-		nbLig = n;
-		nbCol = p;
-		plateau = new char[nbLig][nbCol];
-
-		for(int i = 0; i<nbLig; i++) {
-			for(int j = 0; j<nbCol; j++) {
-				
-
-				plateau[i][j] = 'x';				
-
-			}
-		}
-	}
-	
-
-	
-
-	
-	public void afficher(){
-		System.out.println();
-		for (int i=0; i<nbLig; i++) {
-			for (int j=0; j<nbCol; j++) {
-				
-				System.out.print(" | " + plateau[i][j]);
-				
-				
-				
-			}
-			System.out.println(" |");
-		}
-		System.out.println();
-	}
-}
-
-*/ 
-
-
-
-
-
 package scrabble.matériel;
 
 
 
 public class Plateau {
-
-	Case[][] plateauJeu;
-
-	public Plateau() {
-
-		CreerPlateau();
-
-	}
+    private static final Integer TAILLE = 15;
+    private static final Integer CENTRE = 7;
 
 
+    Case[][] plateauJeu;
 
-	public void CreerPlateau() {
+    public Plateau() {
 
-		plateauJeu = new Case[15][15];
+        creerPlateau();
 
-		for(int i = 0; i < 15; i ++) {
+    }
 
-			for(int j = 0; j < 15; j++) {
 
-				plateauJeu[i][j] = Case.SIMPLE;
 
-			}
+    public void creerPlateau() {
 
-		}
+        plateauJeu = new Case[TAILLE][TAILLE];
 
-	}
+        for(int i = 0; i < TAILLE; i ++) {
+
+            for(int j = 0; j < TAILLE; j++) {
+
+                plateauJeu[i][j] = Case.SIMPLE;
+
+            }
+
+        }
+        plateauJeu[CENTRE][CENTRE] = Case.DEBUT;
+    }
+
 
 }
-		
-
-
-
