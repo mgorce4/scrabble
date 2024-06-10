fc0f2afa3ad0af972cdf80b654dfb4796646bf76
Julie Courty <julielanngoc10@gmail.com>
2024-06-10 10:14:56 +0200

    mise en place ihm

---
+++ src/main/java/scrabble/application/ScrabbleApplicationVue.java
@@ -0,0 +1,16 @@
+package scrabble.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScrabbleApplicationVue extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
