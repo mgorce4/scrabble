56ec25dfe299924729a9f227a9bf1bd429a9ed86
Julie Courty <julielanngoc10@gmail.com>
2024-06-10 10:32:59 +0200

    ça marche max? (code écrit par max mais push sur mon pc)

--- src/main/java/scrabble/application/ScrabbleApplicationVue.java
+++ src/main/java/scrabble/application/ScrabbleApplicationVue.java
@@ -1,16 +1,25 @@
 package scrabble.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ScrabbleApplicationVue extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+        Parent root = FXMLLoader.load(getClass().getResource("/VueScrabble.fxml"));
+        Scene scene = new Scene(root, 1200, 1000);
+
+        primaryStage.setTitle("Scrabble");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
